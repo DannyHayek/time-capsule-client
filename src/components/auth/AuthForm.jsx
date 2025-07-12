7dcb57aaac984c629409bdcd1feb8192d7cab2e0
Danny <dhayek92@gmail.com>
2025-07-12 22:21:05 +0300

    Created navigation to profile page once user signed up

--- src/components/auth/AuthForm.jsx
+++ src/components/auth/AuthForm.jsx
@@ -4,6 +4,7 @@ import EmailInput from './EmailInput'
 import PasswordInput from './PasswordInput'
 import AlterAuth from './AlterAuth'
 import NameInput from './NameInput'
+import { useNavigate } from 'react-router-dom'
 
 const AuthForm = () => {
     const [authType, setAuth] = useState(localStorage.getItem("authType"));
@@ -12,13 +13,25 @@ const AuthForm = () => {
     const [currentPass, setPass] = useState("");
     const [currentName, setName] = useState("");
 
+    const navigate = useNavigate();
 
   const handleInput = () => {
     console.log(currentEmail);
+    localStorage.setItem("currentEmail", currentEmail);
+
     console.log(currentPass);
+    localStorage.setItem("currentPass", currentPass);
+
     if (authType === "Signup") {
       console.log(currentName);
+      localStorage.setItem("currentName", currentName);
     }
+
+    authenticate();
+  }
+
+  const authenticate = () => {
+    navigate("/Profile");
   }
 
 
--- src/components/routes/PageRoutes.jsx
+++ src/components/routes/PageRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import AuthPage from '../../pages/AuthPage/AuthPage'
 import Landing from '../../pages/Landing/Landing'
 import MessageShore from '../../pages/MessageShore/MessageShore'
+import Profile from '../../pages/Profile/Profile'
 
 const PageRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const PageRoutes = () => {
       <Route path="/" element={<Landing />} />
       <Route path="/authPage" element={<AuthPage />} />
       <Route path="/messageShore" element={<MessageShore />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   )
 }
