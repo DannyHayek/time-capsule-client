db18029467c41b341175432a9387b1b9907a0ae1
Danny <dhayek92@gmail.com>
2025-07-13 13:48:57 +0300

    Created bottle a message button to be used on most pages with styling and layout

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,6 +5,7 @@ import PageRoutes from '../../components/routes/PageRoutes';
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Nav from '../../components/shared/Nav';
+import BottleMessageButton from '../../components/shared/BottleMessageButton';
 
 function App() {
   const location = useLocation();
@@ -15,6 +16,7 @@ function App() {
     <div className="App">
       {discluded.every((route) => route !== location.pathname) && <Nav />}
       <PageRoutes />
+      {discluded.every((route) => route !== location.pathname) && <BottleMessageButton />}
     </div>
   );
 }
---
+++ src/pages/BottleMessage/BottleMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BottleMessage = () => {
+  return (
+    <div>BottleMessage</div>
+  )
+}
+
+export default BottleMessage
