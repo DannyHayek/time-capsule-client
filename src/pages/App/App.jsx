809a7f3d4568355b40be2283bc0703cf130279e5
Danny <dhayek92@gmail.com>
2025-07-13 11:57:42 +0300

    Implemented logic to apply nav for most pages except those where it is not neccessary

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,11 +4,16 @@ import Landing from '../Landing/Landing';
 import PageRoutes from '../../components/routes/PageRoutes';
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+import Nav from '../../components/shared/Nav';
 
 function App() {
+  const location = useLocation();
+
+  const discluded = ["/", "/authPage"];
 
   return (
     <div className="App">
+      {discluded.every((route) => route !== location.pathname) && <Nav />}
       <PageRoutes />
     </div>
   );
