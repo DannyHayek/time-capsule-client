8ccf2c55402fd0fa6d5baa0455a3fb244c963ec2
Danny <dhayek92@gmail.com>
2025-07-13 18:48:26 +0300

    Added more layout to message stream cards

--- src/components/messageStream/MessageStreamCard.jsx
+++ src/components/messageStream/MessageStreamCard.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const MessageStreamCard = ({ num, bottled, recieved, status, tag }) => {
   return (
     <div className='flex column space-evenly message-stream-card'>
-        <p>{num}</p>
+        <div className='flex'>
+            <h3>Bottle #{num}</h3>
+            <img src="../../assets/"></img>
+        </div>
+        
 
         <div className='message-stream-info'>
             <p>{bottled}</p>
