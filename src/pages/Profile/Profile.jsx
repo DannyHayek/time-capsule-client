e0abca64b9cc1b187702de7f8d69a258b792c9e9
Danny <dhayek92@gmail.com>
2025-07-13 13:24:15 +0300

    Created basic layout for profile with title components

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ProfileTitle from '../../components/profile/ProfileTitle'
 import './Profile.css';
+import StatCardsSection from '../../components/profile/StatCardsSection';
 
 const Profile = () => {
   return (
     <div>
       <ProfileTitle />
-      
+      <StatCardsSection />
     </div>
   )
 }
