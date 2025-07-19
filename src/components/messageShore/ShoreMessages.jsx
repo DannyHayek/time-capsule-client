7c811cb808de3cff7913866eb974e3fba29e24fa
Danny <dhayek92@gmail.com>
2025-07-19 23:20:48 +0300

    Implemented link to message shore and dynamically generating message cards, some details pending

--- src/components/messageShore/ShoreMessages.jsx
+++ src/components/messageShore/ShoreMessages.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import ShoreMessageCard from './ShoreMessageCard'
 import { useState } from 'react'
 
-const ShoreMessages = () => {
-      const [shoreMessages, setShore] = useState([
-      { num: 22, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
-      { num: 33, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
-      { num: 23, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
-      { num: 55, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
-      { num: 77, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
-      { num: 44, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
-      { num: 12, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
-      { num: 45, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
-      { num: 45, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
-    ])
+const ShoreMessages = ({shoreMessages}) => {
+    //   const [shoreMessages, setShore] = useState([
+    //   { num: 22, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
+    //   { num: 33, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
+    //   { num: 23, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
+    //   { num: 55, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
+    //   { num: 77, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
+    //   { num: 44, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
+    //   { num: 12, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
+    //   { num: 45, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
+    //   { num: 45, user:"user222", bottled: "26/10/2000", recieved: "29/11/2000", status: "Public", tag:"Sad", body:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum" },
+    // ])
 
   return (
     <div className='message-shore-section'>
       {shoreMessages.map((message) => {
-        return <ShoreMessageCard num={message.num} user={message.user} bottled={message.bottled} recieved={message.recieved} status={message.status} tag={message.tag} body={message.body}/>;
+        return <ShoreMessageCard num={message.id} user={message.username} bottled={message.created_at.slice(0,10)} recieved={message.delivery_date} status={message.status} tag={message.tagname} body={message.text}/>;
       })}
     </div>
   )
--- src/pages/MessageShore/MessageShore.jsx
+++ src/pages/MessageShore/MessageShore.jsx
@@ -4,13 +4,30 @@ import ShoreTitle from '../../components/messageShore/ShoreTitle';
 import ShoreTagSelector from '../../components/messageShore/ShoreTagSelector';
 import ShoreMessages from '../../components/messageShore/ShoreMessages';
 import ShoreTags from '../../components/messageShore/ShoreTags';
+import CreateAPI from '../../components/shared/CreateAPI';
+import GetUserID from '../../components/shared/GetUserID';
+import GetToken from '../../components/shared/GetToken';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const MessageShore = () => {
+  const [shoreMessages, setShoreMessages] = useState();
+
+  const getAllMessages = () => {
+    const api = CreateAPI("/guest/message_shore");
+
+    axios.get(api).then(response => setShoreMessages(response.data["payload"]));
+  }
+
+  useEffect(() => {
+      getAllMessages();
+    }, []);
+
   return (
     <div className='flex column message-shore'>
       <ShoreTitle />
       <ShoreTags />
-      <ShoreMessages />
+      {!!shoreMessages && <ShoreMessages shoreMessages = {shoreMessages}/>}
     </div>
   )
 }
