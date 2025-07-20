823ff19b592dc2b588712c5aa66b79435bf00288
Danny <dhayek92@gmail.com>
2025-07-20 17:39:59 +0300

    Implemented surprise capsule hiding and timer on user page

--- src/components/messageStream/MessageStreamCard.jsx
+++ src/components/messageStream/MessageStreamCard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import shareIcon from '../../assets/Icons/share-trans.png'
 import { useNavigate } from 'react-router-dom'
 
-const MessageStreamCard = ({ num, user, bottled, recieved, status, tag, country, body }) => {
+const MessageStreamCard = ({ num, user, bottled, recieved, status, tag, country, body, surprise }) => {
   const navigate = useNavigate();
   const currentMessage = {
     "num": num,
@@ -12,6 +12,43 @@ const MessageStreamCard = ({ num, user, bottled, recieved, status, tag, country,
     "status": status,
     "tag": tag,
     "body": body
+  }
+
+  function dateDiffInDays(a, b) {
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+  }
+
+  if (bottled < recieved && surprise) {
+    const start = new Date(bottled);
+    const end = new Date(recieved);
+
+    return (
+      <div>
+        <div className='flex column space-evenly message-stream-card'>
+        <div className='flex column message-stream-card-title'>
+          <div className='flex'>
+            <h3>Bottle # {num} </h3>
+            <img className='share-icon' src={shareIcon} alt="Share icon"></img>
+          </div>
+            <h5>{country}</h5>
+        </div>
+        
+
+        <div className='message-stream-info'>
+          <article>
+            <h4 className='stream-info-title'>Revealed In</h4>
+            <p>{dateDiffInDays(start, end)} days</p>
+          </article>
+        </div>
+      
+        </div>
+      </div>
+    )
   }
   // console.log(num);
   return (
--- src/components/messageStream/MessageStreamSection.jsx
+++ src/components/messageStream/MessageStreamSection.jsx
@@ -18,7 +18,8 @@ console.log(userMessages)
   return (
     <div className='message-stream-section'>
         {userMessages.map((message) => {
-        return <MessageStreamCard num={message.id} user={message.username} bottled={message.created_at.slice(0,10)} recieved={message.delivery_date} status={message.status} tag={message.tagname} country={message.location} body={message.text}/>;
+          console.log(message.isSurprise);
+        return <MessageStreamCard num={message.id} user={message.username} bottled={message.created_at.slice(0,10)} recieved={message.delivery_date} status={message.status} tag={message.tagname} country={message.location} body={message.text} surprise={message.isSurprise}/>;
       })}
     </div>
   )
