0bb16a9ca2cdb98701492c7d61ba7c15a6fa266e
Danny <dhayek92@gmail.com>
2025-07-21 23:47:42 +0300

    Changed file to image since that is all that works for now

--- src/components/bottleMessage/BottleMessageFile.jsx
+++ src/components/bottleMessage/BottleMessageFile.jsx
@@ -5,7 +5,7 @@ const BottleMessageFile = ({setFile}) => {
   return (
 
     <div className='flex column upload-image-div'>
-      <label>Upload a file...</label>
+      <label>Upload an Image...</label>
       <input onChange={e => setFile(e.target.files[0])} type='file'></input>
     </div>
   )
